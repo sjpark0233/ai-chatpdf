57df0a7942c796f9cc38682d9c5a4e673c126e49
sjpark0233 <sjpark0233@gmail.com>
2025-08-03 01:17:54 +0900

    modified

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from langchain.chains import RetrievalQA
 import streamlit as st
 import tempfile
 import os
+from streamlit_extras.buy_me_a_coffee import button
+
+button(username="sjpark0233", floating=True, width=221)
 
 #제목
 st.title("ChatPDF")
